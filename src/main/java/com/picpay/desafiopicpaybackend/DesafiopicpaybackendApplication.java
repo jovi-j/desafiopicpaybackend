7e73de672e42c3da4e82ef07f9b7f65d56694dcf
Jovi <nean@protonmail.com>
2023-08-20 18:14:07 -0300

    initial commit

---
+++ src/main/java/com/picpay/desafiopicpaybackend/DesafiopicpaybackendApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.desafiopicpaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiopicpaybackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafiopicpaybackendApplication.class, args);
+    }
+
+}
