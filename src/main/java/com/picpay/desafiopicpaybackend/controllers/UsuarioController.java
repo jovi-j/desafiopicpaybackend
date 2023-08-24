fdf8338488234ed24183b9792294a06dbdf58868
Jovi <nean@protonmail.com>
2023-08-24 20:39:47 -0300

    feat: users controller

---
+++ src/main/java/com/picpay/desafiopicpaybackend/controllers/UsuarioController.java
@@ -0,0 +1,23 @@
+package com.picpay.desafiopicpaybackend.controllers;
+
+import java.util.List;
+import com.picpay.desafiopicpaybackend.domain.Usuario;
+import com.picpay.desafiopicpaybackend.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> findAllUsuarios() {
+        return usuarioService.findAllUsuarios();
+    }
+
+}
