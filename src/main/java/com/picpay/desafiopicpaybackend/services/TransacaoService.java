6e9562e76c8c5752971185f573c8f90ae021b1b2
Jovi <nean@protonmail.com>
2023-08-25 02:58:21 -0300

    feat: implemented validation a transition save

--- src/main/java/com/picpay/desafiopicpaybackend/services/TransacaoService.java
+++ src/main/java/com/picpay/desafiopicpaybackend/services/TransacaoService.java
@@ -3,6 +3,9 @@ package com.picpay.desafiopicpaybackend.services;
 import java.util.List;
 
 import com.picpay.desafiopicpaybackend.domain.Transacao;
+import com.picpay.desafiopicpaybackend.domain.Usuario;
+import com.picpay.desafiopicpaybackend.domain.enums.TipoUsuario;
+import com.picpay.desafiopicpaybackend.dtos.TransacaoDTO;
 import com.picpay.desafiopicpaybackend.repositories.TransacaoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +16,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TransacaoService {
+
     @Autowired
     private TransacaoRepository transacaoRepository;
+
+    @Autowired
+    private UsuarioService usuarioService;
 
     public List<Transacao> findTransacoesByRemetente(Long idRemetente) {
         return transacaoRepository.findAllTransacaoByRemetente_Id(idRemetente).orElseThrow();
@@ -24,7 +31,37 @@ public class TransacaoService {
         return transacaoRepository.findAllTransacaoByDestinatario_Id(idDestinatario).orElseThrow();
     }
 
-    public Transacao salvarTransacao(Transacao transacao) {
-        return transacaoRepository.save(transacao);
+    public Transacao salvarTransacao(TransacaoDTO dto) throws Exception {
+        Transacao newTransacao = new Transacao();
+
+        Usuario remetente = usuarioService.findById(dto.remetenteId());
+        Usuario destinatario = usuarioService.findById(dto.destinatarioId());
+
+        newTransacao.setRemetente(remetente);
+        newTransacao.setDestinatario(destinatario);
+
+        validarTransacao(newTransacao);
+
+        transacaoRepository.save(newTransacao);
+
+        return newTransacao;
+    }
+
+    public void validarTransacao(Transacao transacao) throws Exception {
+
+        // RN 001: Usuário do tipo LOJISTA não pode realizar transferências, apenas
+        // recebê-las.
+        if (transacao.getRemetente().getTipoUsuario() == TipoUsuario.LOJISTA) {
+            throw new Exception("Erro: O Usuário é do tipo LOJISTA.");
+        }
+        // RN 002: Usuário com saldo insuficiente não pode realizar transferência.
+        if (transacao.getRemetente().getSaldo().compareTo(transacao.getValor()) < 0) {
+            throw new Exception("Erro: Saldo insuficiente.");
+        }
+        // RN 003: Usuário não pode transferir para si mesmo.
+        if (transacao.getRemetente().equals(transacao.getDestinatario())) {
+            throw new Exception("Erro: O Usuário não pode realizar transferência para si mesmo.");
+        }
+
     }
 }
